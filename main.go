6a4047cc15cc38e681ad7df52beede4cd17f608e
Mihir <mihir.kurdear@gmail.com>
2024-02-19 00:12:57 +0530

    Hello World with chi

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	fmt.Println("Hello World")
+
+	router := chi.NewRouter()
+
+	router.Get("/hello", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
